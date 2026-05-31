ae31a1c3601cc70fdbb70d40d1f110e68db3911b
krugzer <antoine.krings@gmail.com>
2026-05-31 23:15:06 +0200

    ft_write

---
+++ srcs/ft_write.s
@@ -0,0 +1,21 @@
+default rel
+section .text
+global ft_write
+
+;ssize_t	ft_write(int fd, const void buf[count], size_t count)
+;syscall number : 1 (in rax)
+;retval in rax
+ft_write:
+	mov		rax, 1
+	syscall
+	cmp		rax, 0
+	jl		.errno
+	ret
+.errno:
+	neg		rax
+	push	rax
+	call    __errno_location wrt ..plt
+	pop		rcx
+	mov		[rax], rcx
+	mov		rax, -1
+	ret

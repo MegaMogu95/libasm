c26a51889b3003cbe6597a39183512eb567fcc48
krugzer <antoine.krings@gmail.com>
2026-06-04 01:32:55 +0200

    jfkdls

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -5,19 +5,32 @@ extern ft_strlen
 
 ;int ft_atoi_base(char *str, char *base)
 ft_atoi_base:
-	xor		rax, rax						;zero rax to cumulate XOR
 	push	rdi								;keep rdi (str) before calling check_base
+	mov		rdi, rsi
 	call	.check_base						;rax is now 0 or 1
-	pull	rdi								;getting back str in rdi
+	pull	rsi								;getting back str in rsi
 	cmp		rax, 0
 	je		.error							;jump and return 0 in case of error (0 was returned)
 
-.error:
-	ret
-
 ;check_base -> ft_strlen > 2, no duplicates, no +- or whitespace (isspace)
-
+;base in rdi, ret in rax. Doesn't touch rdi (base)
 .check_base:
-	mov		rdi, rsi						;move rsi (base) to rdi (first argument)
 	call	ft_strlen
 	cmp		rax, 2
+	jl		.error
+	call	.check_duplicates
+	cmp		rax, 1
+	jl		.error
+	call	.check_char
+	cmp		rax, 1
+	jl		.error
+
+.check_duplicates:
+	mov		rcx, 0
+	mov		rdx, 1
+	.loop:
+		
+
+.error:
+	xor	rax, rax
+	ret
